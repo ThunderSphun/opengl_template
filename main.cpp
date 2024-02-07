bcf9c69245327c745ebff5a44fa9e59c82dfcb47
ThunderSphun <manderijn2000@gmail.com>
2024-02-07 19:44:06 +0100

    added immensly simple window creation

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	SDL_InitSubSystem(SDL_INIT_VIDEO);
+
+	SDL_Window* window = SDL_CreateWindow("Hello World", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 720, 480, SDL_WINDOW_RESIZABLE);
+
+	SDL_Delay(5000);
+
+	SDL_DestroyWindow(window);
+
 	return 0;
 }
